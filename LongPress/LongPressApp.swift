a5ddbf4086bb414563220f1b9da58e111aff3919
Douglas Jasper <doug.jasper@trios.com>
2025-03-21 08:07:23 -0400

    Initial Commit

---
+++ LongPress/LongPressApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressApp.swift
+//  LongPress
+//
+//  Created by Douglas Jasper on 2025-03-21.
+//
+
+import SwiftUI
+
+@main
+struct LongPressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
